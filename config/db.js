8e822f5a514fee35dd34655d989ab3834274c14c
Maria C Braulino <carol.braulino@hotmail.com>
2022-05-04 00:48:22 -0300

    atualizando senha bd

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production") {
-    module.exports = { mongoURI:"mongodb+srv://carolbraulino:<mcarol12>@cluster0.qstld.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
+    module.exports = { mongoURI:"mongodb+srv://carolbraulino:<q6rdi8x3OeEki9d3>@cluster0.qstld.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
 } else {
     module.exports = { mongoURI: "mongodb://localhost/projetospotify"}
 }
